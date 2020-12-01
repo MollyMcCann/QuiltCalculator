aa688fbb92adad776405f6bf35a81c33874fc33d
Molly Mccann <mollymccann@bellevuecollege.edu>
2020-12-01 12:42:06 -0800

    completing first menu Item

--- QuiltCalculator/Program.cs
+++ QuiltCalculator/Program.cs
@@ -5,68 +5,70 @@ namespace QuiltCalculator
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Quilt Calculator.\n");
             Console.ReadKey();
 
-            int userInput = 0;
-            do
+            bool userInput = true;
+            while (userInput)
             {
-                userInput = MainMenu();
-            } while (userInput != 5);
+                userInput = DisplayMenu();
+            }
+            
 
-            //bool showMenu = true;
-            //while (showMenu)
-            //{
-            //    showMenu = MainMenu();
-            //}
-            //Let's try something else
-
+            
         }
-       static public int MainMenu()
+
+        private static bool DisplayMenu()
         {
-         
-            Console.WriteLine("What would you like to do?");
-            //calculate how many squares (of different demnsions per various sizes of quilt 
+
+
+            Console.Clear();
+            Console.WriteLine("what would you like to do?");
             Console.WriteLine("1) Calculate number of squares needed");
             Console.WriteLine("2) Fabric Measurement Convertor");
             Console.WriteLine("3) Exit");
             Console.Write("\r\nSelect an option: ");
-            var result = Console.ReadLine();
-            return Convert.ToInt32(result);
+           // var result = Console.ReadLine();
 
 
-            //switch (Console.ReadLine())
-            //{
-            //    case "1":
-            //        SquareCount();
-            //        return true;
-            //    case "2": 
-            //        MeasureConvert();
-            //        return true;
-            //    case "3":
-            //        return false;
-            //    default:
-            //        return true;
-            //}
-
-           
-
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    CalculateSquares();
+                    return true;
+                case "2":
+                    FabricMeasurement();
+                    return true;
+                case "3":
+                    return false;
+                default:
+                    return true;
+            }
         }
 
-        private static void SquareCount()
+        private static void FabricMeasurement()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void CalculateSquares()
         {
             Console.Clear();
+            Console.WriteLine(" Lets calculate how many squares");
+            Console.WriteLine("What is the desired length of you quilt? ");
+            decimal lengthSide = decimal.Parse(Console.ReadLine());
+            Console.Write("what is the desires width the width of your quilt: ");
+            decimal widthSide = decimal.Parse(Console.ReadLine());
 
-            Console.WriteLine("how many squares ...");
+            decimal area = lengthSide * widthSide;
+            Console.WriteLine("The area of your quilt is: {0}", area);
+            Console.ReadLine();
         }
-        private static void MeasureConvert()
-        {
-            
-            Console.WriteLine("lets convert a measurement");
 
-        }
     }
-}
-// issue is the bool, something about this is going to make i so we wretun to the main menu each time it is called
+
+ }
+
